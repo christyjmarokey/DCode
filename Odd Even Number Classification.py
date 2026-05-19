3a83a4878a122b277932a909f5f2bf7734f6ebb3
AEmmi4U <aemmiforyou@gmail.com>
2026-05-19 15:58:26 +0200

    Classifying Odd and Even Number

---
+++ Odd Even Number Classification.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 15:49:27 2026
+
+@author: christyjm
+"""
+
+x = 9
+
+if x == 0:
+    print("The number is zero.")
+elif x%2 == 0:
+    print ("The number is even.")
+else:
+    print ("The number is odd.")
