029a873cfafa492e14198dcdcd3621410143b926
AEmmi4U <aemmiforyou@gmail.com>
2026-05-19 15:47:31 +0200

    Basic If-else practice

---
+++ Basic If-else practice.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 15:44:15 2026
+
+@author: christyjm
+"""
+
+a = 200
+b = 33
+
+if a == b:
+    print("a and b are equal")
+    
+elif b > a:
+    print("b is greater than a")
+    
+else:
+    print("a is greater than b")
