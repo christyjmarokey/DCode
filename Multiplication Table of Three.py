6e598169ae7cea67a9106b1d0ecab8d6bf3cd278
AEmmi4U <aemmiforyou@gmail.com>
2026-05-19 20:12:30 +0200

    Printing the multiplication table of 3

---
+++ Multiplication Table of Three.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 20:06:23 2026
+
+@author: christyjm
+"""
+
+
+for i in range (1, 11):
+    j = i * 3
+    print (i, " * ", 3, " = ", j)
